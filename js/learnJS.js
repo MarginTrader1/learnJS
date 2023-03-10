41a1f7680dff23c34bdd2ce13cb0d888455ca088
Mykhailo Statsenko <mihail.prk@gmail.com>
2023-03-10 07:41:51 +0100

    fix some

--- js/learnJS.js
+++ js/learnJS.js
@@ -1,14 +1,10 @@
-const object = { mail: "poly@mail.com", isOnline: true, score: 500 } 
-let propCount = 0;
-  // Change code below this line
-    for (const key in object) {
-  
-      if (object.hasOwnProperty(key)) {
-       propCount += 1;
-      }
-      console.table(propCount)  
-   }
+const message = {
+  name: `Hello`,
+  showMessage() {
+    return `Hello World`;
+  }
+}
 
-   console.table(propCount)
-  
-   
+console.log(message.showMessage);
+
+
