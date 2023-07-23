9b21128d18a57e4b5f2652f7279aed6042da5fb4
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-07-23 16:01:31 +0300

    Update learnJS.js

--- js/learnJS.js
+++ js/learnJS.js
@@ -1,10 +1,12 @@
-const message = {
-  name: `Hello`,
-  showMessage() {
-    return `Hello World`;
-  }
-}
+// Will run first
+console.log("First log");
 
-console.log(message.showMessage);
+setTimeout(() => {
+  // Will run last, after 2000 milliseconds
+  console.log("Second log");
+}, 2000);
+
+// Will run second
+console.log("Third log");
 
 
